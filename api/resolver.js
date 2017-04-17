f363970055eddf456b5f41ad51059c558ddb13e3
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-04-17 16:25:21 -0300

    Adding link and index

--- api/resolver.js
+++ api/resolver.js
@@ -11,7 +11,9 @@ module.exports = (req, res) => {
     //   <iframe src="https://www.google.com/maps/embed/v1/place?key=${API_KEY}&q=${data.q}" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     // `
     body: `
-    <img src="https://maps.googleapis.com/maps/api/staticmap?center=${data.q}&size=600x450&maptype=roadmap&markers=${data.q}&key=${API_KEY}" />
+    <img src="https://maps.googleapis.com/maps/api/staticmap?center=${data.q}&size=300x225&maptype=roadmap&markers=${data.q}&key=${API_KEY}" />
+    <br>
+    <a href="https://mixmax-location.herokuapp.com?q=${data.q}">Live Map View</a>
     `
   })
 }
