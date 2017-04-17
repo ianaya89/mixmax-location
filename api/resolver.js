c8e410584499c2aaf2a0af9744adee83565d5b8c
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-04-17 14:07:11 -0300

    Adding iframe

--- api/resolver.js
+++ api/resolver.js
@@ -8,6 +8,9 @@ module.exports = function(req, res) {
   var width = data.width > 600 ? 600 : data.width;
   var html = data.src;
   res.json({
-    body: `<div> <h1>Current Location</h1> ${html} </div>`
+    body:
+    `
+    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d8283.658953761378!2d-58.4772329663063!3d-34.51162809669516!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sar!4v1492448798460" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    `
   });
 };
